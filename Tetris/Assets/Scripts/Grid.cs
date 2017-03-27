27250643037688d0e7e5785b94941e0e0a359bbf
Unknown <jakobsson.christer@gmail.com>
2017-03-27 22:48:57 +0200

    started a tetris game

---
+++ Tetris/Assets/Scripts/Grid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public static int w = 10;
+    public static int h = 20;
+    public static Transform[,] grid = new Transform[w,h];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static Vector2 RoundVector2(Vector2 v) {
+        return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
+    }
+
+    public static bool InsideBorder(Vector2 pos) {
+        return (int) pos.x >= 0 && (int) pos.x < w && (int) pos.y >= h;
+    }
+}
---
+++ Tetris/Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] Groups;
+
+    // Use this for initialization
+    void Start() {
+        SpawnNext();
+    }
+
+    private void SpawnNext() {
+        int i = Random.Range(0, Groups.Length);
+        Instantiate(Groups[i], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
